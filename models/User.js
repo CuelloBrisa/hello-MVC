ca07fc361517a60abe82a7c3589cae64edb4008e
CuelloBrisa <brisacuello.a@gmail.com>
2020-08-16 21:05:04 -0300

    fist commit

---
+++ models/User.js
@@ -0,0 +1,14 @@
+//models/User.js
+
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    id: Number,
+    name: String,
+    mail: String,
+    birthday: Date
+});
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
---
+++ routes/index.js
@@ -0,0 +1,9 @@
+//routes/Index.js
+const express = require('express');
+const controller = require('../controllers/index');
+const router = express.Router();
+
+router.get('/', controller.home);
+router.get('/search', controller.search)
+
+module.exports = router;
